e017141b0025162ea563440d6c22eaa12b6aa8db
Fabian Gebhart <fabian.gebhart@gmail.com>
2022-08-29 22:04:39 +0200

    ENH: prefer selenium webdriver wait over time.sleep for faster test execution (#10)

--- tests/test_tolino.py
+++ tests/test_tolino.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 from zeit_on_tolino import tolino
+from zeit_on_tolino.web import Delay
 
 
 def test__login(webdriver) -> None:
@@ -15,33 +16,33 @@ def test__login(webdriver) -> None:
 def _delete_last_uploaded_epub(webdriver) -> None:
     # refresh page to ensure menu is closed for further interaction with the page
     webdriver.refresh()
-    time.sleep(3)
+    time.sleep(Delay.small)
 
     # wait for overlay object to be invisible
-    WebDriverWait(webdriver, 10).until(EC.invisibility_of_element_located((By.ID, "splash-screen")))
+    WebDriverWait(webdriver, Delay.medium).until(EC.invisibility_of_element_located((By.ID, "splash-screen")))
     # note, the index=0 indicates that the most recently uploaded epub should be deleted as it is listed first
     index = 0
-    epub_menu = WebDriverWait(webdriver, 10).until(
+    epub_menu = WebDriverWait(webdriver, Delay.medium).until(
         EC.element_to_be_clickable(
             (By.CSS_SELECTOR, f'div[data-test-id="library-myBooks-titles-list-{index}-contextMenu"]')
         )
     )
     epub_menu.click()
-    time.sleep(2)
+    time.sleep(Delay.small)
 
     # click on delete
     for span in webdriver.find_elements(By.TAG_NAME, "span"):
         if span.text == "Löschen":
             span.click()
             break
-    time.sleep(2)
+    time.sleep(Delay.small)
 
     # confirm delete by clicking on 'ok'
     for span in webdriver.find_elements(By.TAG_NAME, "span"):
         if span.text == "OK":
             span.click()
             break
-    time.sleep(2)
+    time.sleep(Delay.small)
 
 
 def test_upload_epub(webdriver, test_epub_path, test_epub_title) -> None:
